1e225ff53b91494cd80627e5b218f0136e2a04e3
Sergey <mukhinserg88@gmail.com>
2020-05-06 21:07:48 +0300

    Add line bar on reserved panel

--- Assets/Scripts/Screeps3D/Tools/Selection/Subpanels/ReservedPanel.cs
+++ Assets/Scripts/Screeps3D/Tools/Selection/Subpanels/ReservedPanel.cs
@@ -4,6 +4,7 @@ using Screeps_API;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using Common;
 
 namespace Screeps3D.Tools.Selection.Subpanels
 {
@@ -11,18 +12,13 @@ namespace Screeps3D.Tools.Selection.Subpanels
     {
         [SerializeField] private TextMeshProUGUI _label;
         [SerializeField] private Image _badge;
+        [SerializeField] private ScaleAxes _meter;
 
         private RoomObject _roomObject;
         private IReserved _reservedObject;
-        public override string Name
-        {
-            get { return "Reserved"; }
-        }
+        public override string Name => "Reserved";
 
-        public override Type ObjectType
-        {
-            get { return typeof(IOwnedObject); }
-        }
+        public override Type ObjectType => typeof(IOwnedObject);
 
         public override bool IsPanelAvailabelForObject(RoomObject roomObject)
         {
@@ -45,9 +41,11 @@ namespace Screeps3D.Tools.Selection.Subpanels
                 return;
             }
             this.Show();
-            _label.text = string.Format("{0}({1:n0})",_reservedObject.ReservedBy.Username , _reservedObject.ReservationEnd - _reservedObject.Room.GameTime);
+            float time = _reservedObject.ReservationEnd - _reservedObject.Room.GameTime;
+            _label.text = string.Format("{0}({1:n0})",_reservedObject.ReservedBy.Username , time);
             _badge.sprite = Sprite.Create(_reservedObject.ReservedBy.Badge,
                     new Rect(0.0f, 0.0f, BadgeManager.BADGE_SIZE, BadgeManager.BADGE_SIZE), new Vector2(.5f, .5f));
+            _meter.SetVisibility(time / Constants.CONTROLLER_RESERVE_MAX);
         }
 
         private void OnTick(long obj)
